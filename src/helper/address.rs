4eab5d75a2ce9f661c7f8501b0fe80f1c10b93f7
mxsr <mxsr+dev@proton.me>
2024-12-22 19:26:43 +0100

    refactor(helper/address): update is_address function for regex initialization
    - Refactored the `is_address` function to use `OnceLock` for lazy initialization of the regex pattern, improving performance by compiling the regex only once.
    - Enhanced documentation for `is_address` to clarify its parameters, return values, and potential panics.
    - Removed the previous implementation of `is_address` to streamline the code.
    - Added examples in the documentation to demonstrate usage.

--- src/helper/address.rs
+++ src/helper/address.rs
@@ -1,4 +1,8 @@
+use std::sync::OnceLock;
+
 use fancy_regex::Regex;
+
+static ADDRESS_REGEX: OnceLock<Regex> = OnceLock::new();
 
 #[must_use]
 pub fn split_address(address: &str) -> (&str, &str) {
@@ -15,6 +19,46 @@ pub fn join_address(sheet_name: &str, address: &str) -> String {
     format!("{sheet_name}!{address}")
 }
 
+/// Checks if the given input string is a valid address format.
+///
+/// The address format is defined by the following regular expression:
+/// `^([^\:\\\?$$$$\/\*]+\!)?(\$?[A-Z]{1,3}\$?[0-9]+)(\:\$?[A-Z]{1,3}\$?[0-9]+)?
+/// $`.
+///
+/// # Parameters
+///
+/// - `input`: A string slice that can be converted to a string reference. This
+///   is the input string to be checked against the address format.
+///
+/// # Returns
+///
+/// Returns `true` if the input string matches the address format, and `false`
+/// otherwise.
+///
+/// # Panics
+///
+/// This function may panic if the regular expression fails to compile. However,
+/// since the regular expression is initialized only once and is hardcoded, this
+/// is unlikely to occur unless there is a bug in the regex itself. The panic
+/// will occur during the first call to this function if the regex is invalid.
+///
+/// # Examples
+///
+/// ```
+/// let valid_address = "$A1";
+/// assert!(is_address(valid_address));
+///
+/// let invalid_address = "invalid_address";
+/// assert!(!is_address(invalid_address));
+/// ```
+pub fn is_address<S: AsRef<str>>(input: S) -> bool {
+    let regex = ADDRESS_REGEX.get_or_init(|| {
+        Regex::new(r"^([^\:\\\?\[\]\/\*]+\!)?(\$?[A-Z]{1,3}\$?[0-9]+)(\:\$?[A-Z]{1,3}\$?[0-9]+)?$")
+            .unwrap()
+    });
+    regex.is_match(input.as_ref()).unwrap()
+}
+
 #[test]
 fn split_address_test() {
     assert_eq!(split_address("A1"), ("", "A1"));
@@ -22,13 +66,6 @@ fn split_address_test() {
     assert_eq!(split_address("sheet1!A1:B2"), ("sheet1", "A1:B2"));
     assert_eq!(split_address("'she!et1'!A1:B2"), ("she!et1", "A1:B2"));
     assert_eq!(split_address(r#"'she"et1'!A1:B2"#), (r#"she"et1"#, "A1:B2"));
-}
-
-pub fn is_address<S: AsRef<str>>(input: S) -> bool {
-    let re =
-        Regex::new(r"^([^\:\\\?\[\]\/\*]+\!)?(\$?[A-Z]{1,3}\$?[0-9]+)(\:\$?[A-Z]{1,3}\$?[0-9]+)?$")
-            .unwrap();
-    re.is_match(input.as_ref()).unwrap()
 }
 
 #[test]
