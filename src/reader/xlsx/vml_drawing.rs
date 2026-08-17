aa6a80f66ff0f6ae629b2a3439d8d1e71bdbcd5b
DESKTOP-U434MT0\hiro <airlain1211@yahoo.co.jp>
2026-08-18 01:22:32 +0900

    bug fix

--- src/reader/xlsx/vml_drawing.rs
+++ src/reader/xlsx/vml_drawing.rs
@@ -4,7 +4,6 @@ use quick_xml::{
 };
 
 use crate::{
-    OleObject,
     structs::{
         Worksheet,
         raw::{
@@ -41,12 +40,11 @@ pub(crate) fn read(
                             .map(|comment| comment.set_shape(obj));
                         comment_index += 1;
                     } else {
-                        let mut ole = OleObject::default();
-                        ole.set_shape(obj);
                         worksheet
                             .ole_objects_mut()
                             .ole_object_mut()
-                            .insert(ole_index, ole);
+                            .get_mut(ole_index)
+                            .map(|ole_obj| ole_obj.set_shape(obj));
                         ole_index += 1;
                     }
                 }
